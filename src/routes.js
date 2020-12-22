350d7edadd86de60370e40ff28ae7be4b451316a
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-22 19:14:54 -0200

    Ajustado ao padrão de codigo standard

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
-const routes = new Router()
+const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.send('ola mundo!')
-})
+  return res.send('ola mundo!');
+});
 
-export default routes
+export default routes;
