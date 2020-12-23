9a018386d4d83aa6c3f81e6cdc41d446472d6f9c
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-23 16:19:24 -0200

    Criado rota do middleware e rota de update usuario

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionCotnroller from './app/controllers/SessionController';
 
+import midConfig from './middlewares/midConfig';
+
 const routes = new Router();
 
 routes.post('/user', UserController.store);
 
 routes.post('/session', SessionCotnroller.store);
+
+routes.use(midConfig);
+routes.post('/user', UserController.update);
 
 export default routes;
