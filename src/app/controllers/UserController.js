b0f7ede5c204651d1fd3e20aaafa7fc7c519cfed
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-22 23:44:44 -0200

    Criado metodo de cadastro de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,6 +3,13 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     const { name, email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({ error: 'Email ja cadastrado!' });
+    }
+
     const user = await User.create({
       name,
       email,
