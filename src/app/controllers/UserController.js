e9cba90563781580468f3ec381d03d5171a5b89c
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-23 16:21:58 -0200

    Criado metodo de alteração de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -18,6 +18,10 @@ class UserController {
 
     return res.status(201).json(user);
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
