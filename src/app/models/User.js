491b66a6257cff10180dd5217a9ef029d733f7b3
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-23 18:16:41 -0200

    Criado campos para guardar token de reset

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -16,6 +16,14 @@ const UserSchema = new Schema({
     required: true,
     select: false,
   },
+  passwordResetToken: {
+    type: String,
+    select: false,
+  },
+  passwordResetExpires: {
+    type: String,
+    select: false,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
