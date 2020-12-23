ddc47422f31d01531d14c0fa9d1a56e441ccf479
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-22 23:22:16 -0200

    Criado model de usuario

---
+++ src/app/models/User.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default model('User', UserSchema);
