daf5fa05940a03e2f064952030e8e53cc169262f
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-22 23:32:20 -0200

    Criado rota de cadastro de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.send('ola mundo!');
-});
+routes.get('/', UserController.store);
 
 export default routes;
