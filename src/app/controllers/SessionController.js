8b6ee1d30300185ec8dffb2c5bb15b4145ab9993
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-23 18:34:49 -0200

    Criado metodo de recuperação de senha do usuário

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,8 @@ import User from '../models/User';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import auth from '../../config/auth';
+import crypto from 'crypto';
+import mailer from '../../email/mailer';
 
 class SessionCotnroller {
   async store(req, res) {
@@ -28,6 +30,40 @@ class SessionCotnroller {
         expiresIn: '7d',
       }),
     });
+  }
+
+  async recover(req, res) {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ erro: 'Nenhum usuário cadastrado com esse email' });
+    }
+
+    const token = crypto.randomBytes(20).toString('hex');
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+
+    await User.findByIdAndUpdate(user._id, {
+      $set: {
+        passwordResetToken: token,
+        passwordResetExpires: now,
+      },
+    });
+
+    mailer.sendMail({
+      to: email,
+      from: 'adminsup@gmail.com',
+      subject: 'Recuperação de senha',
+      text:
+        'Você esqueceu sua senha? Não tem problema, utilize esse token:' +
+        token,
+    });
+
+    return res.json({ message: 'Verifique seu email' });
   }
 }
 
