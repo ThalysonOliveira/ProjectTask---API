92f274c48c7fcc638831baacff8c1949238edb0b
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2020-12-23 15:41:31 -0200

    Criado rota de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionCotnroller from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/user', UserController.store);
+
+routes.post('/session', SessionCotnroller.store);
 
 export default routes;
